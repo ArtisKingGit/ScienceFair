917ac6db25d3f2f1ee474645b29b4abfb14064bf
Claudia Waithaka <claudiawaithaka@Claudias-MacBook-Pro-91.local>
2024-10-07 16:25:00 +0300

    16:24 - 7/10/24

---
+++ account.py
@@ -0,0 +1,133 @@
+from customtkinter import *
+from tkinter import *
+from PIL import Image
+from subprocess import call
+import psycopg2
+
+app = CTk()
+app.geometry("856x645")
+app.resizable(0, 0)
+
+set_appearance_mode("light")
+
+def fetch_username():
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='postgres' password='asdfghj3' host='localhost' port='5432'")
+        cur = conn.cursor()
+
+        # Fetch the first username from the 'names' table
+        cur.execute('SELECT username FROM names LIMIT 10')
+        fetched_username = cur.fetchone()
+
+        if fetched_username:
+            print(f"Fetched username: {fetched_username[0]}")
+            return fetched_username[0]  # Return the username
+        else:
+            print("No username found in the database.")
+            return None
+    
+    except (psycopg2.DatabaseError, psycopg2.Error) as e:
+        print(f"Database error: {e}")
+        return None
+    
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+def fetch_account_name(username):
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='postgres' password='asdfghj3' host='localhost' port='5432'")
+        cur = conn.cursor()
+
+        # Fetch the account name for the given username
+        cur.execute('SELECT username FROM names WHERE username = %s', (username,))
+        account_name = cur.fetchone()
+
+        if account_name:
+            print(f"Fetched account name: {account_name[0]}")
+            cur.execute('DELETE FROM "names" WHERE "username"='admin' AND "pass"='admin@123'')
+            return account_name[0]
+        else:
+            print("No account name found for this username.")
+            return "No account found"
+    
+    except (psycopg2.DatabaseError, psycopg2.Error) as e:
+        print(f"Database error: {e}")
+        return "Error fetching account name"
+    
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+# Sidebar- main
+sidebar_frame = CTkFrame(master=app, fg_color="#2A8C55", width=176, height=650, corner_radius=0)
+sidebar_frame.pack_propagate(0)
+sidebar_frame.pack(fill="y", anchor="w", side="left")
+
+# Rocket image
+logo_img_data = Image.open("logo.png")
+logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(77.68, 85.42))
+CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), anchor="center")
+
+# Dashboard
+analytics_img_data = Image.open("analytics_icon.png")
+analytics_img = CTkImage(dark_image=analytics_img_data, light_image=analytics_img_data)
+CTkButton(master=sidebar_frame, image=analytics_img, text="Dashboard", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(60, 0))
+
+# Feedback
+feedback_img_data = Image.open("feedback_icon.png")
+feedback_img = CTkImage(dark_image=feedback_img_data, light_image=feedback_img_data)
+CTkButton(master=sidebar_frame, image=feedback_img, text="Feedback", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+# Orders
+package_img_data = Image.open("package_icon.png")
+package_img = CTkImage(dark_image=package_img_data, light_image=package_img_data)
+CTkButton(master=sidebar_frame, image=package_img, text="Orders", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+# The order lists
+list_img_data = Image.open("list_icon.png")
+list_img = CTkImage(dark_image=list_img_data, light_image=list_img_data)
+CTkButton(master=sidebar_frame, image=list_img, text="Orders", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+# Returns
+returns_img_data = Image.open("returns_icon.png")
+returns_img = CTkImage(dark_image=returns_img_data, light_image=returns_img_data)
+CTkButton(master=sidebar_frame, image=returns_img, text="Returns", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+# Settings
+settings_img_data = Image.open("settings_icon.png")
+settings_img = CTkImage(dark_image=settings_img_data, light_image=settings_img_data)
+CTkButton(master=sidebar_frame, image=settings_img, text="Settings", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+
+# Account
+person_img_data = Image.open("person_icon.png")
+person_img = CTkImage(dark_image=person_img_data, light_image=person_img_data)
+CTkButton(master=sidebar_frame, image=person_img, text="Account", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(160, 0))
+
+# Main view
+main_view = CTkFrame(master=app, fg_color="#fff", width=680, height=650, corner_radius=0)
+main_view.pack_propagate(0)
+main_view.pack(side="left")
+
+CTkLabel(master=main_view, text="Accounts", font=("Arial Black", 25), text_color="#2A8C55").pack(anchor="nw", pady=(29, 0), padx=10)
+
+CTkLabel(master=main_view, text="Your Account", font=("Arial Black", 18), text_color="#2A8C55").pack(anchor="nw", pady=(29, 0), padx=10)
+
+# Fetch username and account name from the database
+username = fetch_username()
+account_person_img_data = Image.open("person_icon.png")
+account_person_img = CTkImage(dark_image=account_person_img_data, light_image=account_person_img_data)
+
+# Use the fetched account name as the text on the button
+if username:
+    account_name_text = fetch_account_name(username)
+else:
+    account_name_text = "No username found"
+
+CTkButton(master=main_view, image=account_person_img, text=account_name_text, fg_color="#207244", font=("Arial Bold", 14), width=2000, height=70).pack(anchor="nw", pady=(29, 0), padx=10)
+
+app.mainloop()
---
+++ index2.py
@@ -0,0 +1,108 @@
+from customtkinter import *
+from tkinter import messagebox
+from PIL import Image
+import subprocess
+import psycopg2
+
+# Create the main login window
+Login_Form = CTk()
+Login_Form.title("School Bookshop Login")
+Login_Form.geometry("856x645")
+set_appearance_mode("dark")
+# Function to call Dashboard.py and destroy the login window
+def call():
+    Login_Form.destroy()
+    try:
+        subprocess.Popen(["python", "Dashboard.py"])
+    except subprocess.CalledProcessError as e:
+        print("Error executing Dashboard.py:", e)
+
+def call2():
+    Login_Form.destroy()
+    try:
+        subprocess.Popen(["python", "/Users/beginner/Desktop/Advanced Arthur/Python-Advanced-main/index.py"])
+    except subprocess.CalledProcessError as e:
+        print("Error executing index.py:", e)
+
+# Function to handle login attempt
+def login_attempt(username, password):
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='postgres' password='asdfghj3' host='localhost' port='5432'")
+        cur = conn.cursor()
+        
+        # Execute the query to fetch user with given username and password
+        cur.execute('SELECT * FROM names WHERE username = %s AND pass = %s', (username, password))
+        
+        # Fetch the first row (if exists)
+        user = cur.fetchone()
+        
+        if user:
+            messagebox.showinfo("Success", "Successfully logged in!")
+            cur.close()
+            conn.close()
+            
+            # Call the function to open Dashboard.py and destroy current window
+            call()
+        else:
+            messagebox.showerror("Error", "Login failed: Incorrect username or password.")
+        
+    except (psycopg2.DatabaseError, psycopg2.Error) as e:
+        messagebox.showerror("Error", f"Database error: {e}")
+    
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+            
+def keyhandlerfunction(event):
+   try:
+       login_attempt(login_entry.get(), password_entry.get())
+   except Exception as e:
+       print("Error, Failed.", e)
+
+            
+# Sidebar frames and widgets
+sidebar_frame2 = CTkFrame(master=Login_Form, fg_color="transparent", width=526, height=645, corner_radius=10, border_color="#000", border_width=5)
+sidebar_frame2.pack_propagate(0)
+sidebar_frame2.pack(anchor="ne", side="right")
+
+sidebar_frame = CTkFrame(master=Login_Form, fg_color="transparent", width=330, height=645, corner_radius=10)
+sidebar_frame.pack_propagate(0)
+sidebar_frame.pack(anchor="nw", side="left")
+
+lbl_welcome =CTkLabel(master=sidebar_frame2, text="Welcome Back!", font=("Arial",40) )
+lbl_welcome.pack(pady=50, padx = 10)
+
+login_entry = CTkEntry(master=sidebar_frame2, width=300, placeholder_text="Enter username...", corner_radius=10)
+login_entry.pack(pady=10)
+
+password_entry = CTkEntry(master=sidebar_frame2, width=300, placeholder_text="Enter password...", show="*", corner_radius= 10)
+password_entry.pack(pady=10)
+
+btn_login2 = CTkButton(master=sidebar_frame2,hover_color= "#b5b5b5", width=300, height= 40, font=("Arial", 16), text_color="Black", fg_color="#fff", border_width=3,corner_radius=10, border_color="#207244", text="Login", command=lambda: login_attempt(login_entry.get(), password_entry.get()))
+btn_login2.pack(pady=10)
+
+img_google = Image.open("google.png")
+btn_login = CTkButton(master=sidebar_frame2,hover_color= "#b5b5b5", width=300,height= 40, corner_radius=10,text_color="Black", font=("Arial", 16), fg_color="#fff", border_width=3, border_color="#207244", text="Login With Google", image=CTkImage(dark_image=img_google))
+btn_login.pack(pady=10)
+
+img_facebook = Image.open("facebook.png")
+btn_login3 = CTkButton(master=sidebar_frame2,hover_color= "#b5b5b5", width=300, height= 40, font=("Arial", 16), text_color="Black", fg_color="#fff", border_width=3,corner_radius=10, border_color="#207244", text="Login with Facebook", image=CTkImage(dark_image=img_facebook))
+btn_login3.pack(pady=10)
+
+img_apple = Image.open("apple.png")
+btn_login4 = CTkButton(master=sidebar_frame2,hover_color= "#b5b5b5", width=300, height= 40, font=("Arial", 16), text_color="Black", fg_color="#fff", border_width=3,corner_radius=10, border_color="#207244", text="Login with Apple", image=CTkImage(dark_image=img_apple))
+btn_login4.pack(pady=10)
+
+btn_login5 = CTkButton(master=sidebar_frame2,hover_color= "#b5b5b5",  width=300, height= 10, font=("Arial", 16), text_color="Black", fg_color="#fff", border_width=3,corner_radius=100, border_color="#207244", text="Don't have an account? Create an Account", command = call2)
+btn_login5.pack(pady=10)
+
+img_logo_data = Image.open("MuchaTseBle.jpeg")
+img_logo = CTkImage(dark_image=img_logo_data, light_image=img_logo_data, size=(330, 650))
+CTkLabel(master=sidebar_frame, text="", image=img_logo).pack(anchor="center")
+
+Login_Form.bind("<Return>", keyhandlerfunction)
+
+Login_Form.mainloop()
+
